ae0abb01482a855425a58aa32fd4e33d5b05b384
StellaVerkijk <62950143+StellaVerkijk@users.noreply.github.com>
2020-07-29 16:58:45 +0200

    Update get_average_scores_per_label.py
    include 8 annotators

--- get_average_scores_per_label.py
+++ get_average_scores_per_label.py
@@ -127,22 +127,43 @@ def get_dataframe(df, annotator_names):
     dict1 = get_annotator_dict(df, 'labels_'+annotator_names[0])
     dict2 = get_annotator_dict(df, 'labels_'+annotator_names[1])
     dict3 = get_annotator_dict(df, 'labels_'+annotator_names[2])
+    dict4 = get_annotator_dict(df, 'labels_'+annotator_names[3])
+    dict5 = get_annotator_dict(df, 'labels_'+annotator_names[4])
+    dict6 = get_annotator_dict(df, 'labels_'+annotator_names[6])
+    dict7 = get_annotator_dict(df, 'labels_'+annotator_names[7])
+    dict8 = get_annotator_dict(df, 'labels_'+annotator_names[8])
     
     # merge all dictionaries without taking any double ones
     for key, value in dict2.items():
         if key not in dict1:
             dict1[key] = value
-
     for key, value in dict3.items():
         if key not in dict1:
             dict1[key] = value
-    
+    for key, value in dict4.items():
+        if key not in dict1:
+            dict1[key] = value
+     for key, value in dict5.items():
+        if key not in dict1:
+            dict1[key] = value
+     for key, value in dict7.items():
+        if key not in dict1:
+            dict1[key] = value 
+     for key, value in dict8.items():
+        if key not in dict1:
+            dict1[key] = value           
+            
     # create a dataframe from the dictionaries
     new_df = pd.DataFrame.from_dict(dict1, orient = 'index')
     new_df.columns = ['label', 'file_id', 'begin_span', 'end_span', '2nd_label', '2nd_file_id', 'begin_2nd_label', 'end_2nd_label']
     new_df[annotator_names[0]] = 0
     new_df[annotator_names[1]] = 0
     new_df[annotator_names[2]] = 0
+    new_df[annotator_names[3]] = 0
+    new_df[annotator_names[4]] = 0
+    new_df[annotator_names[5]] = 0
+    new_df[annotator_names[6]] = 0
+    new_df[annotator_names[7]] = 0
     
     return(new_df)
 
@@ -177,7 +198,7 @@ def get_average_score_per_label(df, label):
 def main():
     
     # list annotator names
-    annotator_names = ['avelli', 'bos', 'meskers']
+    annotator_names = ['avelli', 'bos', 'katsburg', 'meskers', 'opsomer', 'swartjes', 'vanderpas', 'vervaart']
     
     # read in Quirine's df
     df = pd.read_pickle('token_level_df_all_annotators_all_docs.pkl')
@@ -187,24 +208,66 @@ def main():
     dict_a1 = get_annotator_dict(df, 'labels_'+annotator_names[0])
     dict_a2 = get_annotator_dict(df, 'labels_'+annotator_names[1])
     dict_a3 = get_annotator_dict(df, 'labels_'+annotator_names[2])
+    dict_a4 = get_annotator_dict(df, 'labels_'+annotator_names[3])
+    dict_a5 = get_annotator_dict(df, 'labels_'+annotator_names[4])
+    dict_a6 = get_annotator_dict(df, 'labels_'+annotator_names[5])
+    dict_a7 = get_annotator_dict(df, 'labels_'+annotator_names[6])
+    dict_a8 = get_annotator_dict(df, 'labels_'+annotator_names[7])
     
     # score the annotations in the df
     new_df = find_matches(new_df, annotator_names[0], dict_a1)
     new_df = find_matches(new_df, annotator_names[1], dict_a2)
     new_df = find_matches(new_df, annotator_names[2], dict_a3)
+    new_df = find_matches(new_df, annotator_names[3], dict_a4)
+    new_df = find_matches(new_df, annotator_names[4], dict_a5)
+    new_df = find_matches(new_df, annotator_names[5], dict_a6)
+    new_df = find_matches(new_df, annotator_names[6], dict_a7)
+    new_df = find_matches(new_df, annotator_names[7], dict_a8)
     
     # sum the scores and put in new column
-    new_df['score'] = new_df[annotator_names[0]]+new_df[annotator_names[1]]+new_df[annotator_names[2]]
-    #new_df.to_excel('third_run.xlsx')
+    new_df['score'] = new_df[annotator_names[0]]+new_df[annotator_names[1]]+new_df[annotator_names[2]]+new_df[annotator_names[3]]+new_df[annotator_names[4]]+new_df[annotator_names[5]]+new_df[annotator_names[6]]+new_df[annotator_names[7]]+new_df[annotator_names[8]]
+    new_df.to_excel('excel_with_scores.xlsx')
     
     # extract average scores per label
+    print('average scores domains:')
     print('average score Stemming: '+ str(get_average_score_per_label(new_df, 'Stemming')))
     print('average score Lopen: '+ str(get_average_score_per_label(new_df, 'Lopen')))
+    print('average score Beroep en Werk: '+str(get_average_score_per_label(new_df, 'Beroep')))
+    print('average score Inspanningstolerantie: '+str(get_average_score_per_label(new_df, 'Inspanningstolerantie')))
+    print()
+    print('average scores levels:')
+    print('average score FAC 0: '+ str(get_average_score_per_label(new_df, 'FAC 2')))
     print('average score FAC 1: '+ str(get_average_score_per_label(new_df, 'FAC 1')))
     print('average score FAC 2: '+ str(get_average_score_per_label(new_df, 'FAC 2')))
+    print('average score FAC 3: '+ str(get_average_score_per_label(new_df, 'FAC 2')))
     print('average score FAC 4: '+ str(get_average_score_per_label(new_df, 'FAC 4')))
+    print('average score FAC 5: '+ str(get_average_score_per_label(new_df, 'FAC 5')))
+    print()
+    print('average score STM 0: '+ str(get_average_score_per_label(new_df, 'STM 0')))
+    print('average score STM 1: '+ str(get_average_score_per_label(new_df, 'STM 1')))
+    print('average score STM 2: '+ str(get_average_score_per_label(new_df, 'STM 2')))
+    print('average score STM 3: '+ str(get_average_score_per_label(new_df, 'STM 3')))
+    print('average score STM 4: '+ str(get_average_score_per_label(new_df, 'STM 4')))
+    print('average score STM 5: '+ str(get_average_score_per_label(new_df, 'STM 5')))
+    print()
+    print('average score INS 0: '+ str(get_average_score_per_label(new_df, 'INS 0')))
+    print('average score INS 1: '+ str(get_average_score_per_label(new_df, 'INS 1')))
+    print('average score INS 2: '+ str(get_average_score_per_label(new_df, 'INS 2')))
+    print('average score INS 3: '+ str(get_average_score_per_label(new_df, 'INS 3')))
+    print('average score INS 4: '+ str(get_average_score_per_label(new_df, 'INS 4')))
+    print()
+    print('average score BER 0: '+ str(get_average_score_per_label(new_df, 'BER 0')))
+    print('average score BER 1: '+ str(get_average_score_per_label(new_df, 'BER 1')))
+    print('average score BER 2: '+ str(get_average_score_per_label(new_df, 'BER 2')))
+    print('average score BER 3: '+ str(get_average_score_per_label(new_df, 'BER 3')))
+    print('average score BER 4: '+ str(get_average_score_per_label(new_df, 'BER 4')))
+    print()
+    print('average score remaining labels')
     print('average score stm\_reaction: '+ str(get_average_score_per_label(new_df, 'reaction')))
     print('average score type\_Background: '+ str(get_average_score_per_label(new_df, 'Background')))
+    print('average score view\_Patient: '+ str(get_average_score_per_label(new_df, 'Patient')))
+    print('average score view\_Thirdparty: '+ str(get_average_score_per_label(new_df, 'Third')))
+    print('average score type\_Implicit: '+ str(get_average_score_per_label(new_df, 'Implicit')))
     
 if __name__ == "__main__":
     main()
